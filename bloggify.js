d45a1850f39c6828f3929657688eeabf27645811
Ionică Bizău <bizauionica@gmail.com>
2017-01-27 11:06:02 +0200

    Add production database

--- bloggify.js
+++ bloggify.js
@@ -1,9 +1,65 @@
 "use strict";
-module.exports = require("bloggify-config")(require("./config"), {
+
+const conf = require("bloggify-config");
+const DB_URI = conf.isProduction ? "mongodb://bloggify:keh69T3d1QGlu=M@ds129179.mlab.com:29179/heroku_4h3bmd1g" :  "mongodb://localhost/bloggify-forum";
+
+module.exports = conf({
+    "title": "Bloggify Discussions",
+    "description": "A forum application.",
+    "domain": "http://bloggify-forum.com",
+    "plugins": [
+        "bloggify-custom-assets",
+        "bloggify-mongoose"
+    ],
+    "router": "bloggify-flexible-router",
+    "viewer": null,
+    "config": {
+        "bloggify-custom-assets": {
+            "styles": [
+                "src/css/index.css"
+            ],
+            "server": [
+                "server/index.js"
+            ]
+        },
+        "bloggify-flexible-router": {
+            "errorPages": {
+                "404": "404.ajs",
+                "500": "500.ajs"
+            }
+        },
+        "bloggify-mongoose": {
+            "db": DB_URI,
+            "models": {
+                "User": {
+                    "username": "string",
+                    "email": "string",
+                    "password": "string",
+                    "profile": "object"
+                },
+                "Topic": {
+                    "author": "string",
+                    "title": "string",
+                    "slug": "string",
+                    "body": "string",
+                    "created_at": "date",
+                    "votes": ["string"]
+                },
+                "Comment": {
+                    "author": "string",
+                    "body": "string",
+                    "created_at": "date",
+                    "topic": "string",
+                    "votes": "number"
+                }
+            }
+        }
+    }
+}, {
     server: {
         session: {
             storeOptions: {
-                url: "mongodb://localhost/bloggify-forum",
+                url: DB_URI
             }
         }
     }
