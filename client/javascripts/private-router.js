e4dbd64c32950b132fd52c4179399530b968e508
Ionică Bizău <bizauionica@gmail.com>
2017-02-06 18:41:42 +0200

    Remove alert

--- client/javascripts/private-router.js
+++ client/javascripts/private-router.js
@@ -4,8 +4,6 @@ import ComponentLoader from "./component-loader";
 import AdminView from "./admin";
 import Scores from "./scores";
 
-alert("This is a test. If you see this message, just close it.");
-
 ComponentLoader.register("countdown", <Countdown />);
 ComponentLoader.register("admin-view", <AdminView />);
 ComponentLoader.register("scores", <Scores />);
