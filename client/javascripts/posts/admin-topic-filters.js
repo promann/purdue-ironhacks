0b02ba03a3f49f0ec5c117159a15a978ea6bf3a5
Ionică Bizău <bizauionica@gmail.com>
2017-02-16 21:33:20 +0200

    Add the admin topic filters

---
+++ client/javascripts/posts/admin-topic-filters.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default class TopicFilters extends React.Component {
+
+    refreshTopics () {
+        const uniValue = this.refs.university.value;
+        const hackIdValue = this.refs.hack_id.value;
+        this.props.updateTopics(this.props.topics.filter(topic => {
+            return (topic.metadata.university === uniValue || "All" === uniValue)
+                && (+topic.metadata.hack_id === +hackIdValue || "All" === hackIdValue)
+                ;
+        }));
+    }
+
+    render () {
+        let universityOptions = ["All"]
+          , hackIdOptions = ["All"]
+          ;
+
+        this.props.topics.forEach(topic => {
+            if (!universityOptions.includes(topic.metadata.university)) {
+                universityOptions.push(topic.metadata.university);
+            }
+            if (!hackIdOptions.includes(+topic.metadata.hack_id)) {
+                hackIdOptions.push(+topic.metadata.hack_id);
+            }
+        });
+
+        universityOptions = universityOptions.map((c, i) => {
+            return <option key={i} value={c}>{c.charAt(0).toUpperCase() + c.slice(1)}</option>
+        });
+
+        hackIdOptions = hackIdOptions.map((c, i) => {
+            return <option key={i} value={c}>{c}</option>
+        });
+
+        const universitySelect = <select defaultValue="All" ref="university" onChange={this.refreshTopics.bind(this)}>
+            {universityOptions}
+        </select>;
+
+        const hackIdSelect = <select defaultValue="All" ref="hack_id" onChange={this.refreshTopics.bind(this)}>
+            {hackIdOptions}
+        </select>;
+
+        return <span>
+            {universitySelect}
+            {hackIdSelect}
+        </span>;
+    }
+}
