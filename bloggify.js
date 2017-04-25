4adf8f329cebb32361eb6b8e55ba2de2264444f7
Ionică Bizău <bizauionica@gmail.com>
2017-04-25 07:12:36 +0300

    Working on the new version.

--- bloggify.js
+++ bloggify.js
@@ -3,89 +3,81 @@
 const conf = require("bloggify-config");
 
 // Set the right MongoDB URI (depending on the environment).
-const DB_URI = conf.isProduction
-             ? "mongodb://bloggify:JwRsdG28yZUCVLRBU3IHfGGKlfXyhci3rVo1K0z4h4xjZ1Zf1eC3ZCXiItyMRM8i@ds163758.mlab.com:63758/heroku_l6lb4k6w"
-             : "mongodb://localhost/purdue_ironhacks"
-             ;
+const DB_URI = process.env.MONGODB_URI
+if (!DB_URI) {
+    console.error(">>>> Please provide the MongoDB URI. Set the MONGODB_URI environment variable.");
+}
 
 module.exports = conf({
     // Application metadata
-    "title": "IronHacks",
-    "description": "",
+    title: "IronHacks"
+  , description: "Hack for inovation and join the open data movement.",
 
     // The production domain
-    "domain": "http://www.ironhacks.com",
+  , domain: "http://www.ironhacks.com"
 
     // Core plugins (which are initialized before the others)
-    "corePlugins": [
-        "bloggify-mongoose",
-    ],
+  , corePlugins: [
+        "bloggify-mongoose"
+    ]
 
     // Application plugins
-    "plugins": [
-        "bloggify-sendgrid",
-        "bloggify-custom-assets",
-        "bloggify-github-login"
-    ],
+  , plugins: [
+        "bloggify-sendgrid"
+      , "bloggify-custom-assets"
+      , "bloggify-github-login"
+    ]
 
     // The application router
-    "router": "bloggify-flexible-router",
+  , router: "bloggify-flexible-router"
 
     // We do not have a blog page, so we do not need a Bloggify viewer at all
-    "viewer": null,
-
-    // Plugins config in development
-    "devConfig": {
-        "bloggify-github-login": {
-            "githubClient": "c26d485f95fb06c9002d",
-            "githubSecret": "22cb7e7b89a41b2e65338e4382909a504aa0ca0d"
-        }
-    },
+  , viewer: null
 
     // Plugins configuration
-    "config": {
+  , config: {
 
         // Custom application assets
         "bloggify-custom-assets": {
-            "styles": [
+            styles: [
                 "app/assets/stylesheets/index.css"
-            ],
-            "server": [
+            ]
+          , server: [
                 "app/server/index.js"
             ]
-        },
+        }
 
         // The application router
-        "bloggify-flexible-router": {
-            "controllers_dir": "app/controllers",
-            "routes_dir": "app/routes",
-            "error_pages": {
-                "404": "404.ajs",
-                "500": "500.ajs",
-                "bad_csrf": "422.ajs"
+      , "bloggify-flexible-router": {
+            controllers_dir: "app/controllers"
+          , routes_dir: "app/routes"
+          , error_pages: {
+                404: "404.ajs"
+              , 500: "500.ajs"
+              , bad_csrf: "422.ajs"
             }
-        },
+        }
 
         // Login with GitHub
-        "bloggify-github-login": {
-            "githubClient": "45168d125a3ba1f26fe7",
-            "githubSecret": "1f4b54ed8fd52dacd84daa8a8a5e81129e858d62"
-        },
+      , "bloggify-github-login": {
+            githubClient: process.env.GITHUB_CLIENT
+          , githubSecret: process.env.GITHUB_SECRET
+        }
 
         // Connect to the MongoDB database
-        "bloggify-mongoose": {
-            "db": DB_URI,
-            "models_dir": "app/models"
-        },
+      , "bloggify-mongoose": {
+            db: DB_URI
+          , models_dir: "app/models"
+        }
 
         // Send emails
-        "bloggify-sendgrid": {
-            "key": "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"
+      , "bloggify-sendgrid": {
+            key: process.env.SENDGRID_KEY
         }
     }
 }, {
-    // Session information
-    server: {
+    cms_methods: false
+  , server: {
         session: {
             storeOptions: {
                 url: DB_URI
