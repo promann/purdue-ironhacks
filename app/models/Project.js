f21581ec7566701bbdc16654bf074bf4c5c43e75
Jia Lin Cheoh <jialin327@hotmail.com>
2017-08-10 22:26:47 -0400

    Add the Project model

---
+++ app/models/Project.js
@@ -0,0 +1,12 @@
+module.exports = {
+    username: {
+        type: String,
+        index: true
+    },
+    name: {
+        type: String,
+        index: true
+    },
+    description: String,
+    fork: Boolean
+};
