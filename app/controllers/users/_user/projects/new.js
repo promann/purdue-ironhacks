38c77da5a11e149f8c71f2cbb42c98dbe87deac9
Jia Lin Cheoh <jialin327@hotmail.com>
2017-08-10 23:20:16 -0400

    Add header and footer in the project pages.
    Also, deprecate the /view path and use /project/:project_name instead.

--- app/controllers/users/_user/projects/new.js
+++ app/controllers/users/_user/projects/new.js
@@ -1,6 +1,7 @@
 const Bloggify = require("bloggify")
     , Project = Bloggify.models.Project
     , Session = require("../../../Session")
+    , slug = require("slug")
     ;
 
 exports.post = (ctx, cb) => {
@@ -9,11 +10,12 @@ exports.post = (ctx, cb) => {
         return ctx.redirect("/");
     }
 	const data = ctx.data;
+	data.name = slug(data.name);
 	data.username = user.username;
 	new Project(data).save((err, project) => {
 		if (err) {
 			return cb(err);
 		}
-		ctx.redirect(`/users/${project.username}/projects/view/${project.name}`);
+		ctx.redirect(`/users/${project.username}/projects/${project.name}`);
 	});
 };
