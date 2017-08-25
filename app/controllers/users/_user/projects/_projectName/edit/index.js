f061853cf3cfde0952364b6f00ed72fe0966114b
Ionică Bizău <bizauionica@gmail.com>
2017-08-25 17:15:01 +0300

    Trigger template creation

---
+++ app/controllers/users/_user/projects/_projectName/edit/index.js
@@ -0,0 +1,4 @@
+module.exports = (ctx, cb) => {
+    ctx.projectsEditor = true
+    cb()
+}
--- app/controllers/users/_user/projects/new.js
+++ app/controllers/users/_user/projects/new.js
@@ -5,17 +5,18 @@ const Bloggify = require("bloggify")
     ;
 
 exports.post = (ctx, cb) => {
-	const user = Session.isAuthenticated(ctx);
+    const user = Session.isAuthenticated(ctx);
     if (!user) {
         return ctx.redirect("/");
     }
-	const data = ctx.data;
-	data.name = slug(data.name);
-	data.username = user.username;
-	new Project(data).save((err, project) => {
-		if (err) {
-			return cb(err);
-		}
-		ctx.redirect(`/users/${project.username}/projects/${project.name}`);
-	});
+    const data = ctx.data;
+    data.name = slug(data.name);
+    data.username = user.username;
+    new Project(data).save((err, project) => {
+        if (err) {
+            return cb(err);
+        }
+        Bloggify.emit("project:create-template", data)
+        ctx.redirect(`/users/${project.username}/projects/${project.name}`);
+    });
 };
