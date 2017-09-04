1b9f4d01060cf89924334a82bc40b3421c930300
Jia Lin Cheoh <jialin327@hotmail.com>
2017-09-04 15:03:35 -0400

    Use the editor branch name

--- scripts/heroku-deploy.sh
+++ scripts/heroku-deploy.sh
@@ -1,6 +1,6 @@
 echo "Create the heroku-deploy"
 git checkout -B heroku-deploy
-git merge master
+git merge editor
 rm -rf .bloggify/bundle-cache
 npm run bundle
 npm run purify:homepage
@@ -11,5 +11,5 @@ git add . -A
 git commit -m 'Bundle and deploy' .
 
 git push heroku heroku-deploy:master -f
-git checkout master
+git checkout editor
 git branch -D heroku-deploy
