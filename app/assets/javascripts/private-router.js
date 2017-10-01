cdb077bd9d16cca38c32025333f12d437b948dab
Ionică Bizău <bizauionica@gmail.com>
2017-10-01 01:05:40 -0400

    Expose the view-project in the private router

--- app/assets/javascripts/private-router.js
+++ app/assets/javascripts/private-router.js
@@ -4,8 +4,10 @@ import ComponentLoader from "./component-loader";
 import AdminView from "./admin";
 import Scores from "./scores";
 import ProjectEditor from "./project-editor";
+import ViewProject from "./view-project";
 
 ComponentLoader.register("timeline", <Timeline />);
 ComponentLoader.register("admin-view", <AdminView />);
 ComponentLoader.register("scores", <Scores />);
 ComponentLoader.register("project-editor", <ProjectEditor />);
+ComponentLoader.register("view-project", <ViewProject />);
