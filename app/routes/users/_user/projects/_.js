d9da8014f10620f1270b536e3841e69db48c20bd
Ionică Bizău <bizauionica@gmail.com>
2017-10-07 13:01:41 -0400

    Hide the edit button, fix a minor error

--- app/routes/users/_user/projects/_.js
+++ app/routes/users/_user/projects/_.js
@@ -1,3 +1,13 @@
+// Disable the editor if the forum didn't start yet
+exports.use = (ctx, cb) => {
+	if (new Date() < Bloggify.models.Settings.HACK_TYPES[ctx.user.profile.hack_type].start_date) {
+		ctx.redirect("/timeline")
+		return
+	}
+	cb()
+}
+
+
 module.exports = ctx => {
     const Project = Bloggify.models.Project
     return Project.find({
