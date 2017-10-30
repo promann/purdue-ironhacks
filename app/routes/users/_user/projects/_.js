4c831b8071ef33cc610e769298b7b6f3b793aca4
Ionică Bizău <bizauionica@gmail.com>
2017-10-30 17:23:26 +0200

    Sort the projects by name

--- app/routes/users/_user/projects/_.js
+++ app/routes/users/_user/projects/_.js
@@ -22,7 +22,7 @@ module.exports = ctx => {
     const Project = Bloggify.models.Project
     return Project.find({
         username: ctx.selected_user.username
-    }).then(projects => {
+    }).sort({ name: 1 }).then(projects => {
         return { projects }
     })
 };
