29b20d63bef9c314b864bef504e30291b2b51e8e
Ionică Bizău <bizauionica@gmail.com>
2017-10-30 06:24:18 +0200

    Replace tabs with spaces

--- scripts/create-projects.js
+++ scripts/create-projects.js
@@ -14,26 +14,26 @@ Bloggify.onLoad(err => {
     Bloggify.log("Finding the users...")
 
     // 1. Get the list of users
-	Bloggify.models.User.find().then(users => {
-		const limitProjectCreation = pLimit(1)
-		// 2. For each user
-		return Promise.all(users.map(user => {
-			// 3. Create the 3 projects (phase2, 3, 4)
-			return Promise.all(PHASES.map(phase => {
-				return limitProjectCreation(() => {
-					Bloggify.log(`Creating the project for @${user.username}, ${phase}`)
-					return Bloggify.services.projects.create({
-						name: `webapp_${phase}`
-					  , username: user.username
-					  , phase
-					}).then(() => {
-						Bloggify.log(`Created the project for @${user.username}, ${phase}`)
-					}).catc(err => {
-						Bloggify.log(new Error(`Failed to creating the project for @${user.username}, ${phase}`))
-						Bloggify.log(err, "error")
-					})
-				})
-			}))
-		}))
-	}).catch(Bloggify.error)
+    Bloggify.models.User.find().then(users => {
+        const limitProjectCreation = pLimit(1)
+        // 2. For each user
+        return Promise.all(users.map(user => {
+            // 3. Create the 3 projects (phase2, 3, 4)
+            return Promise.all(PHASES.map(phase => {
+                return limitProjectCreation(() => {
+                    Bloggify.log(`Creating the project for @${user.username}, ${phase}`)
+                    return Bloggify.services.projects.create({
+                        name: `webapp_${phase}`
+                        , username: user.username
+                        , phase
+                    }).then(() => {
+                        Bloggify.log(`Created the project for @${user.username}, ${phase}`)
+                    }).catc(err => {
+                        Bloggify.log(new Error(`Failed to creating the project for @${user.username}, ${phase}`))
+                        Bloggify.log(err, "error")
+                    })
+                })
+            }))
+        }))
+    }).catch(Bloggify.error)
 })
