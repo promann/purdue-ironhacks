4f6dc06597b38b8439c29b034a6d0774697ff01a
Ionică Bizău <bizauionica@gmail.com>
2018-03-23 15:36:35 +0200

    Fix #69. Close the preview endpoint

--- app/routes/preview/_user/_projectName/_filepath*/_.js
+++ app/routes/preview/_user/_projectName/_filepath*/_.js
@@ -1,7 +1,8 @@
 module.exports = ctx => {
+    ctx.end("")
+    return false
     // /preview/:user/:project/index.html
     //0 1       2     3        4
-    debugger
     ctx.params.filepath = ctx.url.pathname.split("/").slice(4).join("/")
     Bloggify.services.projects.streamFile(ctx);
     return false
