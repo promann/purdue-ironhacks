b4d068fe30e81543486759b81288711eac794856
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2018-04-22 11:09:03 -0400

    Added new score logic to the dashboard
    New personal table added
    new styling added

--- app/assets/javascripts/scores/generalTable.js
+++ app/assets/javascripts/scores/generalTable.js
@@ -8,15 +8,43 @@ const TITLE = {
   FEEDBACK : "INDIVIDUAL FEEDBACK",
   COMPETITORS : "GENERAL SCORE"
 }
-const DIMENSION_NAMES = ["Technology", "Functionality", "InfoVis", "Novelty"]
-const DIMENSION_DESCRIPTION = ["Tech requirements: You are expected to meet all 10 technological requirements specified in the challenge description (www.ironhacks.com/task). All requirements are equally weighted. The more more requirements you meet the better", "Error rating: We evaluate the quality of your code by counting the errors and diving it by the total number of lines of code. The lower the value the better your code.", "System affordance: Does the application offer recognizable elements and interactions that can be understood by the user? ", "Adding new data sets definitely makes your app stand (take out the three letters) out from the rest. We evaluate how successfully you implement additional open datasets: How relevant are they for you're the application? How novel is the visualization of this data? We evaluate each dataset individually and average it across all datasets."]
-const MEASURE_TYPE_DESCRIPTION = ["Error rating", "Number of technology", "Number of user requirements met", "Usability point achieved"]
+const TABLES_NAMES = ["Technology", "Analytics", "Visualization"]
+const TABLE_DESCRIPTIONS = [
+  'Below, you find the performance of the software code of your app! As a programmer it is important to write flawless code! Also keep in mind that a programmer has to comply to the technological requirements! Check out how well you do!',
+  'Below, you can find out how well you master the analytical expectations of the app user! Remember, the wanted to find the optimal solution! How close did you get in mastering the analytical challenges? Check out below',
+  'Below, you can find out tackle the visualization of the data and analytics in your app. Remember, that there very creative ways to visualize metrics. Check out below how your app tackles the visualization task, and how advanced you are in visualization data. Our goal is to turn you into a visualization champion!'  
+]
+const DIMENSION_NAMES = {
+  Technology: ['Error rating', 'Number of technology requirements met'],
+  Analytics: ['Quality of rankings', 'Functionality', 'Variable variety'],
+  Visualization: ['Map requirements', 'Chart variety', 'Usability points achieved', 'Novelty of the mandatory parameters on the map', 'Novelty of the additional data sets on the map', 'Novelty of the mandatory parameters on the charts', 'Novelty of the additional data sets on the charts']
+}
+const DIMENSION_DESCRIPTIONS = {
+  Technology: [
+    'We evaluate the quality of your code by counting the errors and dividing it by the total number of lines of code. The lower the value, the better your code.',
+    ' You are expected to meet all 10 technological requirements specified in the challenge description (www.ironhacks.com/task). All requirements are equally weighted. The more requirements you meet, the better.'],
+  Analytics: [
+    'As specified in the task (www.ironhacks.com/task), you were asked to correctly rank the districts with respect to three parameters (distance, safety, afforability); this measures accounts whether you correctly find the 10 best districts for each variable individually  and all jointly (ranking considering all three variables jointly). The value describes how many correct ranks you have idenified for all 4 ranking dimensions. ',
+    "Adding the possibility to export the resutls makes your results actionable for other user's purposes. Therefor, we evaluate if your current app presents successfully the function to export the rankings previously calculated.",
+    "Adding more decision parameters may increase the analytical functionality of the tool! There are many variables that you can extract from the eligible datasets (www.ironhacks.com/task). This measure 'metrics variable'  captures how many variables you have tried to use in relation to all variables available in the eligable datasets."],
+  Visualization: [
+    'In the challenge description, we specified a two map requirements. You map should visualize the boroughs and you position the map from a future  resident of New York City. The value measures how well you tackle these requirements.', 
+    "There are varies ways to visualize data. From barcharts to more novel multidimensional charts. The more variety have the more advanced your visualization, the greater the value displayed.",
+    "Our team of info vis experts focuses on two core aspects of information visualization usability: System affordance (Does the application offer recognizable elements and interactions that can be understood by the user?) Cognitive workload (Would a potential user have to memorize a lot of information to carry the task?) You can achieve a score from 0 to 100. All three dimensions are equally weighted.",
+    "Representing the mandatory parameters in new ways on the map definitely makes your app standing out from the rest. We will how well you visualize the mandatory datasets (distance, safety, affordability) on your map. The more unique (e.g.interactive) the more advanced your app. For the implication score you can achieve a score from 0 to 100. We evaluate each parameter individually and then average the score across all mandatory variables that you use.", 
+    "Adding new data sets on the map definitely makes your app standing out from the rest. We evaluate how successfully you implement additional datasets.  Again, the more interactive the more unique (e.g. interactive) your visualization of the datasets in the map, the better. For the implication score you can achieve a score from 0 to 100. We evaluate each dataset individually and then average the score across all datasets that you use.",
+    "Representing the mandatory parameters in new ways on the charts definitely makes your app standing out from the rest. We evaluate how successfully you implement the mandatory parameters. The more unique (e.g. multi-dimensional) your chart visualization, the better your visualization performance. For the implication score you can achieve a score from 0 to 100. We evaluate each variable individually and then average the score across all mandatory variables that you use.", 
+    "Visualizing new data sets on the charts definitely makes your app standing out from the rest. We evaluate how successfully you visualize additional data from the eligable list of data sets (www.ironhacks.com/task). The more unique (e.g. multi-dimensional) your chart visualization, the better. For the implication score you can achieve a score from 0 to 100. We evaluate each dataset individually and then average the score across all datasets that you use.",
+  ]
+}
 const DIMENSION_SCORES = {
   'Technology': 'tech_score',
   'Functionality': 'func_score',
   'InfoVis': 'info_vis_score',
   'Novelty': 'novel_score'
 }
+
+
 
 export default class GeneralTable extends React.Component {
   
@@ -45,44 +73,54 @@ export default class GeneralTable extends React.Component {
         </th>
         if(this.props.viewType == VIEW_INDIVIDUAL){
           //Drawing the individual score table
-          const rows = []
-          for (var i = 0; i < 4; i++) {
-            const currentDimension = DIMENSION_SCORES[DIMENSION_NAMES[i]]
-            rows.push({
-              DIMESION_NAMES: DIMENSION_NAMES[i],
-              DIMENSION_DESCRIPTION: DIMENSION_DESCRIPTION[i],
-              MEASURE_TYPE_DESCRIPTION: MEASURE_TYPE_DESCRIPTION[i],
-              SCORE: this.props.personalScore[currentDimension]
+          var headers = []
+          var rows = []
+          const tableContent = []
+          for (var i = 0; i < TABLES_NAMES.length; i++) {
+            headers =[TABLES_NAMES[i], TABLE_DESCRIPTIONS[i], 'Your evaluation']
+            for (var j = 0; j < DIMENSION_NAMES[TABLES_NAMES[i]].length; j++) {
+              rows.push({
+                DIMENSION_NAME: DIMENSION_NAMES[TABLES_NAMES[i]][j],
+                DIMENSION_DESCRIPTION: DIMENSION_DESCRIPTIONS[TABLES_NAMES[i]][j],
+                SCORE: 0
+              })
+            }
+
+            tableContent.push({
+              headers: headers,
+              rows: rows
             })
+            rows = []
           }
-          const individualScoreData = rows.map((row) => 
-          <div className="score-table table-wrapper" key={row.DIMESION_NAMES.toString()}>
+          console.log(tableContent)
+          const individualScoreData = tableContent.map((content) => 
+          <div className="score-table table-wrapper" key={content.headers[0].toString()}>
             <div className="table-scroll">
               <table>
                 <thead >
                   <tr>
-                    <th colSpan={4}>
-                      {row.DIMESION_NAMES}
+                    <th>
+                      {content.headers[0]}
+                    </th>
+                    <th>
+                      {content.headers[1]}
+                    </th>
+                    <th>
+                      {content.headers[2]}
                     </th>
                   </tr>
-                  {this.props.headers}
                 </thead>
                 <tbody>
-                   <tr>
-                      <td rowSpan={3}>{row.DIMESION_NAMES}</td>
-                      <td>{row.DIMENSION_DESCRIPTION}</td>
-                      <td >{row.MEASURE_TYPE_DESCRIPTION}</td>
-                      <td rowSpan={3}>{row.SCORE}</td>
-                   </tr>
-                   <tr>
-                      <td>{row.DIMENSION_DESCRIPTION}</td>
-                      <td >{row.MEASURE_TYPE_DESCRIPTION}</td>
-                   </tr>
-                   <tr>
-                      <td>{row.DIMENSION_DESCRIPTION}</td>
-                      <td >{row.MEASURE_TYPE_DESCRIPTION}</td>
-                   </tr>
-                   </tbody>
+                  {
+                    content.rows.map((row) => 
+                      <tr key={row.DIMENSION_DESCRIPTION.toString()}>
+                        <td >{row.DIMENSION_NAME}</td>
+                        <td >{row.DIMENSION_DESCRIPTION}</td>
+                        <td>{row.SCORE}</td>
+                      </tr>
+                    )
+                  }
+                  </tbody>
                 </table>
               </div>
             </div>
--- app/assets/javascripts/scores/index.js
+++ app/assets/javascripts/scores/index.js
@@ -11,6 +11,72 @@ import GeneralTable from './generalTable'
 // Constants
 const VIEW_INDIVIDUAL = 'FEEDBACK';
 const VIEW_COMPETITORS = 'COMPETITORS';
+
+const PHASE_0_MESSAGE = <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+  <p><strong>Welcome to your personal dashboard!</strong></p>
+  <p>You are on your personalized feedback page. On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization.</p>
+  <p> Currently, there are no results yet available since Phase 1 has not been completed. After the evaluation phase 1, you should immediately return here and review your results. You are evaluations are private and confidential. Do not share them with others! Stick to the Purdue's honor's code!</p>
+</div>
+const TREATMENT_1_TEXT = {
+  PHASE_1_MESSAGE : <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+    <p><strong>Welcome to your personal dashboard!</strong> You are on your personalized feedback page. On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization. Below are the results for Phase 1.</p>
+    <p><strong>How to read those results:</strong> There are three different scrowable tables with a range of metrics that measure the quality of your submission in your current phase. Keep pushing to increase the quality of your app. The better the quality the higher your reward eventually. Remember that your evaluations are <strong>private and confidential</strong>. Do not share them with others. Remember the Purdue's honors code.</p>
+  </div>,
+  AFTER_PHASE_1_MESSAGE: <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+    <p><strong>Welcome back to your personal dashboard!</strong> You are on your personalized feedback page. On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization. Below are the results for Phase X. You can view the results of prior phases by using the slider in the slide bar above.</p>
+    <p>How to read those results: There are three different scrowable tables with a range of metrics that measure the quality of your submission in your current phase. Keep pushing to increase the quality of your app. The better the quality the higher your reward eventually.</p>
+    <p>Remember that your evaluations are private and confidential.</p> 
+    </div>
+}
+
+const TREATMENT_2_TEXT = {
+  PHASE_1_MESSAGE : <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+    <p><strong>Welcome back to your personal dashboard!</strong></p>
+    <p>On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization. Below are the results for Phase 1.</p>
+    <p>There are two view: "your personal feedback" (private and confidential) and "your competitors" (showing what other's are doing).</p>
+    <p><strong>How to read your personal feedback.</strong> There are three different scrowable tables with a range of metrics that measure the quality of your submission in your current phase. Keep pushing to increase the quality of your app. The better the quality the higher your reward eventually.</p>
+    <p>Remember that your evaluations are private and confidential. Nobody else can see it. And you shouldn't share it with others.</p>
+    <p>Check out your competitors: Also, please take a look at your fellow participants’ solutions. Just click on the tap "My competitors" above and you can see other's apps. It’s a great way to learn how to improve your performance and gear up for the next round! Clck on the link to check out other's code!.</p>
+  </div>,
+  AFTER_PHASE_1_MESSAGE: <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+    <p><strong>Welcome back to your personal dashboard!</strong></p> 
+    <p>On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization. Below are the results for Phase X. You can view the results of prior phases by using the slider in the slide bar above.</p>
+    <p>There are two view: "your personal feedback" (private and confidential) and "your competitors" (showing what other's are doing).</p>
+    <p><strong>How to read your personal feedback.</strong> There are three different scrowable tables with a range of metrics that measure the quality of your submission in your current phase. Keep pushing to increase the quality of your app. The better the quality the higher your reward eventually.</p>
+    <p>Remember that your evaluations are private and confidential. Nobody else can see it. And you shouldn't share it with others.  </p>
+    <p>Check out your competitors: Also, please take a look at your fellow participants’ solutions. Just click on the tap "My competitors" above and you can see other's apps. It’s a great way to learn how to improve your performance and gear up for the next round! Click on the link to check out other's code!</p>
+    </div>
+}
+
+const TREATMENT_3_TEXT = {
+  PHASE_1_MESSAGE : <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+    <p><strong>Welcome back to your personal dashboard!</strong></p> 
+    <p>On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization. Below are the results for Phase 1.</p>
+    <p>There are two view: <strong>"your personal feedback"</strong> (private and confidential) and <strong>"your competitors"</strong> (showing what other's are doing).</p>
+    <p><strong>How to read your personal feedback.</strong> There are three different scrowable tables with a range of metrics that measure the quality of your submission in your current phase. Keep pushing to increase the quality of your app. The better the quality the higher your reward eventually.</p>
+    <p>Remember that your evaluations are private and confidential. Nobody else can see it. And you shouldn't share it with others.</p>
+    <p><strong>Check out your competitors:</strong> Also, please take a look at your fellow participants’ solutions. Just click on the tap <strong>"My competitors"</strong> above and you can see other's apps. It’s a great way to learn how to improve your performance and gear up for the next round! Click on the link to check out other's code! You also find ranking (leaderboard) of your competitors. We use perecentile scores to rank the app. A percentile rank achieved by  you indicates the percentage of participants in your hacking contest who achieved a lower performance than you. </p>
+  </div>,
+  AFTER_PHASE_1_MESSAGE: <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+    <p><strong>Welcome back to your personal dashboard!</strong></p>
+    <p>On this page, you can review the evaluations for your app in a very detailed way for each category: Technology, Analytics, and Visualization. Below are the results for Phase X. You can view the results of prior phases by using the slider in the slide bar above.</p>
+    <p>There are two view: "your personal feedback" (private and confidential) and "your competitors" (showing what other's are doing).</p>
+    <p>How to read your personal feedback. There are three different scrowable tables with a range of metrics that measure the quality of your submission in your current phase. Keep pushing to increase the quality of your app. The better the quality the higher your reward eventually.</p>
+    <p>Remember that your evaluations are private and confidential. Nobody else can see it. And you shouldn't share it with others. </p>
+    <p>Check out your competitors: Also, please take a look at your fellow participants’ solutions. Just click on the tap "My competitors" above and you can see other's apps. It’s a great way to learn how to improve your performance and gear up for the next round!  lick on the link to check out other's code! You also find ranking (leaderboard) of your competitors. We use perecentile scores to rank the app. A percentile rank achieved by  you indicates the percentage of participants in your hacking contest who achieved a lower performance than you.</p>
+    </div>
+}
+
+const DASHBOARD_TEXT = {
+  0: TREATMENT_1_TEXT,
+  1: TREATMENT_2_TEXT,
+  2: TREATMENT_3_TEXT
+}
+
+const CONFIDENTIALITY_MESSAGE = <div style={{'textAlign': 'justify', 'marginTop' : '30px', 'marginBottom' : '30px'}}>
+  <p><strong>CONFIDENTIALITY NOTE: This information is confidential and should not be shared with other participants in your hack! Follow the rules of IronHacks and do not share this information.</strong></p>
+</div>
+
 //Main Component
 export default class App extends React.Component {
   constructor (props) {
@@ -34,6 +100,15 @@ export default class App extends React.Component {
     this.state.currentUser.profile.hack_id = 2
   }
   render(){
+    //Select the text according with the phase
+    var text = <div></div>
+    if(this.state.personalScore.length == 0){
+      text = PHASE_0_MESSAGE
+    }else if(this.state.personalScore.length == 1){
+      text = DASHBOARD_TEXT[this.state.currentUser.profile.hack_id].PHASE_1_MESSAGE
+    }else{
+      text = DASHBOARD_TEXT[this.state.currentUser.profile.hack_id].AFTER_PHASE_1_MESSAGE
+    }
     const viewIndividual = this.state.currentView === VIEW_INDIVIDUAL;
     const viewCompetitors = this.state.currentView === VIEW_COMPETITORS;
     const showGeneralScoreButton = this.state.currentUser.profile.hack_id == 0
@@ -52,29 +127,30 @@ export default class App extends React.Component {
         <div className="row">
           <div className="col-md-3">
             <img src={this.state.currentUser.profile.picture} id="profilePicture"/>
+            <CalendarHeatmap
+              startDate={new Date('2018-03-01')}
+              endDate={new Date('2018-05-31')}
+              classForValue={this.githubClassForValue}
+              values={values}
+            />
           </div>
         <div className="col-md-7 offset-md-1">
           <div className="phases-div">
             <ReactBootstrapSlider step={1} max={5} min={1} orientation="horizontal" id="phaseSlider"
               ticks={[1, 2, 3, 4, 5]}
               ticks_labels={['Phase 1', 'Phase 2', 'Phase 3', 'Phase 4', 'Phase 5']}
-              
               value={this.state.currentPhase}
               change={this.onSliderChange}
             />
           </div>
+          <div>
+            {text}
+          </div>
         </div>  
         </div>
         <div className="row">
-          <div className="col-md-3">
-                  <CalendarHeatmap
-                    startDate={new Date('2018-03-01')}
-                    endDate={new Date('2018-05-31')}
-                    classForValue={this.githubClassForValue}
-                    values={values}
-                  />
-          </div>
-          <div className="col-md-9">
+          <div className="col-md-9 offset-md-3"
+              style={{'display': this.state.personalScore.length > 0 ? 'inline-block' : 'none'}}>
             <div className="row">
               <div className="col-md-6">
                 <div>
@@ -125,14 +201,6 @@ export default class App extends React.Component {
   }
   calculateHeaders(){
     if(this.state.currentView == VIEW_INDIVIDUAL){
-      return(
-        <tr>
-          <th>Perfomance Dimension</th>
-          <th>Description</th>
-          <th>Measure type</th>
-          <th>Your Result</th>
-        </tr>
-      );
     }else{
       if(this.state.currentUser.profile.hack_id == 1){
         return(
@@ -187,9 +255,12 @@ export default class App extends React.Component {
       Actions.get("scores.getPersonalScores")
           .then(scores => {
             console.log(scores)
-              this.setState({personalScore: scores})
+              this.setState({personalScore: scores)
+            if(scores.length != 0){
+              //No scores available, we are still in phase 1.
               //Once we get the projects from the user, we identify  the treatment, and then ask for the adition data, if it needed.
               this.getTreatmentData(this.state.currentUser.profile.hack_id)
+            }
           })
   }
   pullGeneralOS(){
