62472ef728544d228a0404bd5d288e70bb16b909
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2018-05-29 18:08:35 -0500

    added commit link

--- app/assets/javascripts/project-editor/index.js
+++ app/assets/javascripts/project-editor/index.js
@@ -108,7 +108,7 @@ export default class App extends React.Component {
         phase2: "https://purdue.ca1.qualtrics.com/jfe/form/SV_54rzA3pmM7ojfTf",
         phase3: "https://purdue.ca1.qualtrics.com/jfe/form/SV_5BDyrHjKSiYI3sN",
         phase4: "https://purdue.ca1.qualtrics.com/jfe/form/SV_8vJ87qGAiE7EL1b",
-        phase5: "https://purdue.ca1.qualtrics.com/jfe/form/SV_bQK4wA367xloVLL",
+        phase5: "https://purdue.ca1.qualtrics.com/jfe/form/SV_4JybKpVu3Cn6ODH",
       }
     }
     //Setting the "read-only state"
